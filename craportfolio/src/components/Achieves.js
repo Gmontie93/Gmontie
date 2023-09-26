b172f0add71cbe0ad571bb27f8c1971a4a2315da
Geovanny <geovannymontiel16@gmail.com>
2023-09-25 22:28:28 -0700

    Exercise 22

---
+++ craportfolio/src/components/Achieves.js
@@ -0,0 +1,23 @@
+const Achieves = () => {
+    return (  
+        <div class="row" id="achieves">
+
+
+        <div class="col-sm-8 indent1">
+            <h1 class="display-4 indent1">Achievements</h1>
+            <h3>Dean's list:</h3>
+            <ul class="mt-4 px-5">
+                <li>Fall 2020 </li>
+                <li>Spring 2020</li>
+                <li>Fall 2021 </li>
+            </ul>
+           
+             
+         
+        </div>
+       
+        </div>
+    );
+}
+ 
+export default Achieves;
